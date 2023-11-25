77ccc9ece308f8e9786b4100e8e7de4a2a5cd5d1
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2023-11-25 15:01:48 +0530

    html and functions updated
    included different data and script and function and added dropdown

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import pandas as pd
 from flask import  Flask,render_template
 
 app=Flask(__name__)
-
+car=pd.read_csv("Cleaned Car.csv")
 @app.route('/')
 def index():
-    return render_template('index.html')
+    companies = sorted(car['company'].unique())
+    # .unique() returns an array of unique values in that column.
+    # sorted() sorts those unique values alphabetically or numerically.
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique(),reverse=True)
+    #This line extracts the 'year' column from the DataFrame car, retrieves unique values, and sorts them in reverse order (from highest to lowest).
+
+    fuel_type = sorted(car['fuel_type'].unique())
+
+    return render_template('index.html',companies=companies,car_models=car_models,year=year,fuel_type=fuel_type)
+
 
 if __name__=="__main__":
     app.run(debug=True)
-    ###41.51
+    ###56.47
